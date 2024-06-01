ff90c47feb1e9f57d0beb7967c3cf708b6d834d4
Peter Brodersen <peter@ter.dk>
2024-06-01 06:05:20 +0200

    Create FlatGeobuf file from aggregated table

--- import/import.sh
+++ import/import.sh
@@ -4,3 +4,12 @@ wget https://download.geofabrik.de/europe/denmark-latest.osm.pbf -O denmark-late
 # Remember to create schema osmetymology
 # set $PGDATABASE to database name
 osm2pgsql -d "$PGDATABASE" -O flex -S jsonb.lua -s denmark-latest.osm.pbf
+psql -f aggregate.sql
+
+# Create aggregated file
+FGBFILE="../www/data/aggregate.fgb"
+if [ -f "$FGBFILE" ] ; then
+    rm -- "$FGBFILE"
+fi
+
+ogr2ogr ../www/data/aggregate.fgb PG:dbname=penguin -oo TABLES=osmetymology.ways_agg -select "name, name:etymology, name:etymology:wikipedia, name:etymology:wikidata, geom"
