d5068caa22191e100d3bdfad657e1a8898fe12b3
Peter Brodersen <peter@ter.dk>
2024-06-03 00:25:45 +0200

    Split by municipalities

--- www/lookup.php
+++ www/lookup.php
@@ -2,14 +2,19 @@
 require("connect.inc.php");
 header("Content-Type: application/json");
 $name = (string) ($_GET['term'] ?? '');
+$searchname = preg_replace('_[^[:alnum:]]_u', '', mb_strtolower($name));
 $result = [];
 if (strlen($name) < 2) {
 	print json_encode($result);
 	exit;
 }
 
-$q = $dbh->prepare('SELECT id, name, "name:etymology:wikidata" FROM osmetymology.ways_agg WHERE name ILIKE ? LIMIT 10');
-$q->execute([$name . '%']);
+$q = $dbh->prepare('
+	SELECT ow.id, ow.name, ow."name:etymology:wikidata", m.navn 
+	FROM osmetymology.ways_agg ow
+	inner join osmetymology.municipalities m on ow.municipality_code = m.kode
+	WHERE searchname LIKE ? LIMIT 10');
+$q->execute([$searchname . '%']);
 foreach($q AS $row) {
 	$result[] = $row;
 }
