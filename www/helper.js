c500736537d1e6b3a246c63a07913c26099627bf
Peter Brodersen <peter@ter.dk>
2024-06-04 14:38:17 +0200

    Update when hash changes (back/forward in browser)

--- www/helper.js
+++ www/helper.js
@@ -166,3 +166,8 @@ function updateWikidataHTML() {
   });
   return true;
 }
+
+addEventListener("hashchange", (event) => {
+  var starttext = decodeURIComponent(window.location.hash.substring(1))
+  doSearch(starttext);
+});
