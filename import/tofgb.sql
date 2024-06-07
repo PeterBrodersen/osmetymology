e8198373d93f2084135ec69a6ffa36faadee4e05
Peter Brodersen <peter@ter.dk>
2024-06-07 10:56:35 +0200

    Better import, look up gender

---
+++ import/tofgb.sql
@@ -0,0 +1,7 @@
+-- ogr2ogr convert from database to FlatGeobuf file
+SELECT ow.id, ow.name AS streetname, ow."name:etymology", ow."name:etymology:wikidata", ow."name:etymology:wikipedia", ow.municipality_code, m.navn AS municipality_name, w."name" AS wikidata_label, w.description AS wikidata_description, COALESCE(gendermap.gender,'') AS gender, ow.geom
+FROM osmetymology.ways_agg ow
+INNER JOIN osmetymology.municipalities m on ow.municipality_code = m.kode
+LEFT JOIN osmetymology.wikidata w ON ow."name:etymology:wikidata" = w.itemid
+LEFT JOIN (VALUES ('Q6581072', 'female'), ('Q6581097', 'male'), ('Q1052281', 'female'), ('Q2449503', 'male')) as gendermap (itemid, gender) ON w.claims#>'{P21,0}'->'mainsnak'->'datavalue'->'value'->>'id' = gendermap.itemid
+ORDER BY ow.name, m.navn
--- www/lookup.php
+++ www/lookup.php
@@ -14,21 +14,57 @@ if ($search) {
 $searchname = preg_replace('_[^[:alnum:]]_u', '', mb_strtolower($streetname));
 $result = [];
 
+function getColumns () {
+	$columns = [
+		'ow.id',
+		'ow.name AS streetname',
+		'ow.way_ids',
+		'ow.way_ids[1] AS sampleway_id',
+		'ow."name:etymology"',
+		'ow."name:etymology:wikidata"',
+		'ow."name:etymology:wikipedia"',
+		'm.navn AS municipalityname',
+		'w."name" AS wikilabel',
+		'w.description AS wikidescription',
+		'gendermap.gender',
+		'ST_X(ST_Centroid(ow.geom)) AS centroid_longitude',
+		'ST_Y(ST_Centroid(geom)) AS centroid_latitude'
+
+	];
+	$columnList = implode(', ', $columns);
+	return $columnList;
+}
+
+function getQuerystring($type) {
+	$columns = getColumns();
+	$where = '';
+	if ($type == 'searchnamelike') {
+		$where = 'WHERE searchname LIKE ?';
+	} elseif ($type = 'itemid') {
+		$where = 'WHERE "name:etymology:wikidata" = ?';
+	}
+	$querystring = <<<EOD
+	SELECT $columns
+	FROM osmetymology.ways_agg ow
+	INNER JOIN osmetymology.municipalities m on ow.municipality_code = m.kode
+	LEFT JOIN osmetymology.wikidata w ON ow."name:etymology:wikidata" = w.itemid
+	LEFT JOIN (VALUES ('Q6581072', 'female'), ('Q6581097', 'male'), ('Q1052281', 'female'), ('Q2449503', 'male')) as gendermap (itemid, gender) ON w.claims#>'{P21,0}'->'mainsnak'->'datavalue'->'value'->>'id' = gendermap.itemid
+	$where
+	ORDER BY ow.name, m.navn
+	LIMIT 1000
+	EOD;
+	die($querystring);
+	return $querystring;
+}
+
 function findStreetName($searchname)
 {
 	global $dbh;
 	if (strlen($searchname) < 2) {
 		return false;
 	}
-	$q = $dbh->prepare('
-		SELECT ow.id, ow.name AS streetname, ow.way_ids, ow.way_ids[1] AS sampleway_id, ow."name:etymology", ow."name:etymology:wikidata", ow."name:etymology:wikipedia", m.navn AS municipalityname, w."name" AS wikilabel, w.description AS wikidescription, ST_X(ST_Centroid(geom)) AS centroid_longitude, ST_Y(ST_Centroid(geom)) AS centroid_latitude
-		FROM osmetymology.ways_agg ow
-		inner join osmetymology.municipalities m on ow.municipality_code = m.kode
-		LEFT JOIN osmetymology.wikidata w ON ow."name:etymology:wikidata" = w.itemid
-		WHERE searchname LIKE ?
-		ORDER BY ow.name, m.navn
-		LIMIT 1000
-	');
+	$querystring = getQuerystring('searchnamelike');
+	$q = $dbh->prepare($querystring);
 	$q->setFetchMode(PDO::FETCH_ASSOC);
 	$q->execute([$searchname . '%']);
 	$result = $q->fetchAll();
@@ -41,15 +77,8 @@ function findStreetsFromItem($itemid)
 	if (!preg_match('_^Q\d+$_', $itemid)) {
 		return false;
 	}
-	$q = $dbh->prepare('
-		SELECT ow.id, ow.name AS streetname, ow.way_ids, ow.way_ids[1] AS sampleway_id, ow."name:etymology", ow."name:etymology:wikidata", ow."name:etymology:wikipedia", m.navn AS municipalityname, w."name" AS wikilabel, w.description AS wikidescription, ST_X(ST_Centroid(ow.geom)) AS centroid_longitude, ST_Y(ST_Centroid(ow.geom)) AS centroid_latitude
-		FROM osmetymology.ways_agg ow
-		inner join osmetymology.municipalities m on ow.municipality_code = m.kode
-		LEFT JOIN osmetymology.wikidata w ON ow."name:etymology:wikidata" = w.itemid
-		WHERE "name:etymology:wikidata" = ?
-		ORDER BY ow.name, m.navn
-		LIMIT 1000
-	');
+	$querystring = getQuerystring('itemid');
+	$q = $dbh->prepare($querystring);
 	$q->setFetchMode(PDO::FETCH_ASSOC);
 	$q->execute([$itemid]);
 	$result = $q->fetchAll();
