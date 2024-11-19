15c20d2dbbf82af9dddf9e50155034f7ba0bdf55
Peter Brodersen <peter@ter.dk>
2024-11-19 04:00:52 +0100

    Stats, TODO

--- import/updatestatsfile.php
+++ import/updatestatsfile.php
@@ -9,8 +9,8 @@ print date("H:i:s") . ": Creating stats" . PHP_EOL;
 
 $totalroads = $dbh->query('SELECT COUNT(*) FROM osmetymology.ways_agg')->fetchColumn();
 $uniquenamedroads = $dbh->query('SELECT COUNT(DISTINCT name) FROM osmetymology.ways_agg')->fetchColumn();
-$uniqueetymologywikidata = $dbh->query('SELECT COUNT(DISTINCT "name:etymology:wikidata") FROM osmetymology.ways_agg')->fetchColumn();
-$localwikidataitems = $dbh->query('SELECT COUNT(*) FROM osmetymology.wikidata')->fetchColumn();
+$uniqueetymologywikidata = $dbh->query('SELECT COUNT(DISTINCT "name:etymology:wikidata") FROM osmetymology.ways_agg')->fetchColumn(); // :TODO: split/expand multiple name:etymology:wikidata records in same value, e.g. "Q44412;Q491280"
+$localwikidataitems = $dbh->query('SELECT COUNT(*) FROM osmetymology.wikidata')->fetchColumn(); // :TODO: Only check up against actual values in ways_agg, not extra content
 $importfiletime = NULL;
 if (file_exists($statefile)) {
     if (preg_match('/^timestamp=(.*)$/m', file_get_contents($statefile), $match)) {
@@ -27,7 +27,7 @@ $stats = [
 ];
 file_put_contents($statsjsonfile, json_encode($stats));
 
-function getMunicipalityStats() {
+function getMunicipalityStats() { // :TODO: split/expand multiple name:etymology:wikidata records in same value, e.g. "Q44412;Q491280"
 	global $dbh;
 	$querystring = <<<EOD
 	WITH dist AS (
@@ -61,6 +61,7 @@ function strtofloat($scalar) {
 }
 
 $municipalitystats = getMunicipalityStats();
+// :TODO: Add date and other information; put municipalities in their own array
 file_put_contents($municipalityjsonfile, json_encode($municipalitystats));
 
 
